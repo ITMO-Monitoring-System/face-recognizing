8960b6154a47901deca10548d3bbf91fc6916a62
Lev Metskovskii <levfromleon2@gmail.com>
2025-12-19 01:34:13 +0300

    Some changes in retina

--- face_service/core/retina_embending.py
+++ face_service/core/retina_embending.py
@@ -2,22 +2,34 @@ import cv2
 import numpy as np
 from insightface.app import FaceAnalysis
 
-#ctx_id = -1 - CPU
-#ctx_id = 0 - GPU
+_app: FaceAnalysis | None = None
 
-app = FaceAnalysis(allowed_modules=["detection", "recognition"])
-app.prepare(ctx_id=-1, det_size=(640, 640))
 
-#лицо в эмбендинг
+def get_app() -> FaceAnalysis:
+    global _app
+    if _app is None:
+        _app = FaceAnalysis(allowed_modules=["detection", "recognition"])
+        _app.prepare(ctx_id=-1, det_size=(640, 640))
+    return _app
+
+
 def get_face_embeddings(img_bgr: np.ndarray):
-    faces = app.get(img_bgr)              # детект + выравнивание внутри
+    app = get_app()   # <-- инициализация происходит ТОЛЬКО тут
+    faces = app.get(img_bgr)
+
     out = []
     for f in faces:
-        emb = f.normed_embedding.astype(np.float32)#нормирую вектор
+        emb = f.normed_embedding.astype(np.float32)
         bbox = f.bbox.astype(int).tolist()
-        kps  = f.kps.astype(float).tolist()#ключевые точки на лице
-        out.append({"embedding": emb, "bbox": bbox, "kps": kps, "det_score": float(f.det_score)})
+        kps = f.kps.astype(float).tolist()
+        out.append({
+            "embedding": emb,
+            "bbox": bbox,
+            "kps": kps,
+            "det_score": float(f.det_score),
+        })
     return out
+
 
 if __name__ == "__main__":
     img = cv2.imread("image.jpg")
