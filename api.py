60655ce9666fef718f7e7fc75420556a9284f8ff
Lev Metskovskii <levfromleon2@gmail.com>
2025-12-22 18:34:38 +0300

    int

--- api.py
+++ api.py
@@ -105,14 +105,14 @@ class OutCtx:
 # Lecture control API (multi-queue)
 # -----------------------------
 class LectureStartIn(BaseModel):
-    lecture_id: str = Field(..., min_length=1)
+    lecture_id: int
     in_amqp_url: str = Field(..., min_length=1)
     in_queue: str = Field(..., min_length=1)
     threshold: float = 0.1
 
 
 class LectureStopIn(BaseModel):
-    lecture_id: str = Field(..., min_length=1)
+    lecture_id: int
 
 
 # -----------------------------
